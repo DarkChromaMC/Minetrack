f8bf57b7f4eabc0d0a6200c2689c43fac37b1e21
Chroma <68839442+DarkChromaMC@users.noreply.github.com>
2025-06-16 03:32:25 +1200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,12 @@
 # Exit on any error
 set -e
 
-# Take ownership of the volume mount point
-# This ensures the 'minetrack' user can write to it
+# This script is now run as ROOT
+# It now has the permission to change ownership of the /data volume
+echo "Taking ownership of /data..."
 chown -R minetrack:minetrack /data
+echo "Ownership set."
 
-# Execute the original command (tini -- node main.js)
-exec "$@"
+# Now, we step down from root and execute the original CMD as the 'minetrack' user.
+# 'gosu' is a lightweight tool for this.
+exec gosu minetrack "$@"
